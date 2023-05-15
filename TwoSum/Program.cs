8ce55fe443efa329e752e4879a091b95b2b88431
Daniil Yakovlev <yakovlev@bimcl.ru>
2023-05-15 23:44:03 +0500

    feat: add TwoSum app

---
+++ TwoSum/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace TwoSum
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var solution = new Solution();
+            solution.TwoSum(new []{2,7,11,15}, 9);
+        }
+    }
+}
---
+++ TwoSum/Solution.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace TwoSum
+{
+    public class Solution
+    {
+        public int[] TwoSum(int[] nums, int target)
+        {
+
+            int[] arr = new int[nums.Length];
+            
+            for (int i = 0; i < nums.Length; i++)
+            {
+                arr[i] =  Array.IndexOf(nums,nums[i]);
+            }
+            
+            return arr;
+        }
+    }
+}
